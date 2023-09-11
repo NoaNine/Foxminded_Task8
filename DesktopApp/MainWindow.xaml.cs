359ca9106df932510cb1802163ba9c77b61ba0e9
Oleksandr Havlytsky <havlytskyi@gmail.com>
2023-09-11 15:21:59 +0300

    attempt to display data

--- DesktopApp/MainWindow.xaml.cs
+++ DesktopApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Windows;
+using University.DAL;
 using University.DAL.Models;
 using University.DAL.Repositories;
 
@@ -12,15 +13,30 @@ namespace DesktopApp
         private readonly IRepository<Student> _studentRepository;
         private readonly IRepository<Teacher> _teacherRepository;
         private readonly IRepository<Group> _groupRepository;
+        private readonly UniversityContext _universityContext;
 
         public MainWindow(IRepository<Course> courseRepository, IRepository<Student> studentRepository, 
-            IRepository<Teacher> teacherRepository, IRepository<Group> groupRepository)
+            IRepository<Teacher> teacherRepository, IRepository<Group> groupRepository, UniversityContext universityContext)
         {
             InitializeComponent();
+
             _courseRepository = courseRepository;
             _studentRepository = studentRepository;
             _teacherRepository = teacherRepository;
             _groupRepository = groupRepository;
+            _universityContext = universityContext;
+
+
+            //var listCourses = _courseRepository.GetAll();
+
+            //treeWiewCourses.ItemsSource = listCourses;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = _courseRepository.GetAll();
         }
     }
 }
--- University.DAL/UniversityContext.cs
+++ University.DAL/UniversityContext.cs
@@ -9,6 +9,7 @@ public class UniversityContext : DbContext
     public DbSet<Student> Students { get; set; }
     public DbSet<Group> Groups { get; set; }
     public DbSet<Course> Courses { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
 
     public UniversityContext(DbContextOptions<UniversityContext> options) : base(options)
     {
