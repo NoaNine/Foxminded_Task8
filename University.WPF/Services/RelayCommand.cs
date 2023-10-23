0a82aea2365704ccefc264789684ab9b72d7804d
Oleksandr Havlytsky <havlytskyi@gmail.com>
2023-10-23 15:38:27 +0300

    Fixed command

--- University.WPF/Services/RelayCommand.cs
+++ University.WPF/Services/RelayCommand.cs
@@ -5,28 +5,28 @@ namespace University.WPF.Services;
 
 public class RelayCommand : ICommand
 {
-    private Action<object> execute;
-    private Func<object, bool> canExecute;
+    private Action<object> _execute;
+    private Predicate<object> _canExecute;
 
     public event EventHandler CanExecuteChanged
     {
-        add { CommandManager.RequerySuggested += value; }
-        remove { CommandManager.RequerySuggested -= value; }
+        add => CommandManager.RequerySuggested += value; 
+        remove => CommandManager.RequerySuggested -= value; 
     }
 
-    public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+    public RelayCommand(Action<object> execute, Predicate<object> canExecute)
     {
-        this.execute = execute ?? throw new ArgumentNullException("execute");
-        this.canExecute = canExecute;
+        _execute = execute ?? throw new ArgumentNullException("execute");
+        _canExecute = canExecute ?? throw new ArgumentNullException("canExecute");
     }
 
     public bool CanExecute(object parameter)
     {
-        return canExecute == null || canExecute(parameter);
+        return _canExecute(parameter);
     }
 
     public void Execute(object parameter)
     {
-        execute(parameter);
+        _execute(parameter);
     }
 }
