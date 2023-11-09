72c1e98bc7b1ef992ba3edd3593edde0d612b681
Oleksandr Havlytsky <havlytskyi@gmail.com>
2023-11-09 15:56:03 +0200

    Added display student list for groups in treeview

--- University.WPF/ViewModel/HomeViewModel.cs
+++ University.WPF/ViewModel/HomeViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Xaml.Behaviors;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
 using University.DAL.Models;
 using University.DAL.UnitOfWork;
 using University.WPF.Services.Navigator;
@@ -11,7 +14,7 @@ class HomeViewModel : BaseViewModel
 {
     public ObservableCollection<Course> Courses {  get; private set; }
 
-    public HomeViewModel(INavigator navigator, IUnitOfWork unitOfWork) : base(navigator, unitOfWork)
+    public HomeViewModel(INavigator navigator, IUnitOfWork unitOfWork) : base(navigator, unitOfWork) //TODO set a default page
     {
         LoadData();
     }
@@ -19,15 +22,25 @@ class HomeViewModel : BaseViewModel
     private void LoadData() 
     {
         Courses = new ObservableCollection<Course>(UnitOfWork.GetRepository<Course>().GetAll());
+        LoadDataCourse();
+    }
+
+    private void LoadDataCourse()
+    {
         foreach (var course in Courses)
         {
             course.Groups = (ICollection<Group>)UnitOfWork.GetRepository<Group>().GetAll(g => g.CourseId == course.Id);
-            foreach (var group in course.Groups)
-            {
-                group.Students = (ICollection<Student>)UnitOfWork.GetRepository<Student>().GetAll(s => s.GroupId == group.Id);
-                group.Course = UnitOfWork.GetRepository<Course>().GetByID(group.CourseId);
-                group.Tutor = UnitOfWork.GetRepository<Teacher>().GetByID(group.Id);
-            }
+            LoadDataGroup(course);
+        }
+    }
+
+    private void LoadDataGroup(Course course)
+    {
+        foreach (var group in course.Groups)
+        {
+            group.Students = (ICollection<Student>)UnitOfWork.GetRepository<Student>().GetAll(s => s.GroupId == group.Id);
+            group.Course = UnitOfWork.GetRepository<Course>().GetByID(group.CourseId);
+            group.Tutor = UnitOfWork.GetRepository<Teacher>().GetByID(group.Id);
         }
     }
 }
