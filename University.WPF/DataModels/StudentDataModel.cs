85a79c9d6175fbed153a1666afb2aaa122412cca
Oleksandr Havlytsky <havlytskyi@gmail.com>
2023-11-16 00:00:44 +0200

    Created a new model wpf layer

--- University.WPF/DataModels/StudentDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using University.WPF.Services;
-
-namespace University.WPF.DataModels;
-
-public class StudentDataModel : ObservableObject //TODO delete
-{
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-}
---
+++ University.WPF/Models/GroupDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using University.WPF.Models.Base;
+
+namespace University.WPF.Models
+{
+    internal class GroupDTO : BaseModel
+    {
+    }
+}
